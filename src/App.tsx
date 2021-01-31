1971f465c45fbee296f9febc60cec1db840ed9f1
Lula Crackudo <lucasfonsecard@icloud.com>
2021-01-31 07:31:45 -0300

    add styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React from "react";
+import GlobalStyles from "./styles/global"
 
-const App: React.FC = () => {
-	return <h1> Hello World</h1>;
-};
+const App: React.FC = () => (
+	<>
+		<h1> Hello World</h1>;
+		<GlobalStyles />
+	</>
+);
 
 export default App;
