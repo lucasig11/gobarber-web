ed45daa89e1561a4954b9805dfdd849eca3fa144
lucas <lucasfonsecard@icloud.com>
2021-04-28 04:21:40 -0300

    add loading spinner to button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,20 @@
 import React, { ButtonHTMLAttributes } from 'react';
+import Spinner from 'react-bootstrap/Spinner';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Container } from './styles';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+	loading?: boolean;
+};
 
-const Button: React.FC<ButtonProps> = ({ children, ...rest }) => (
-	<Container type="button" {...rest}>
-		{children}
+const Button: React.FC<ButtonProps> = ({ children, loading, ...rest }) => (
+	<Container type="button" disabled={loading} {...rest}>
+		{loading ? (
+			<Spinner size="sm" animation="border" variant="dark" />
+		) : (
+			children
+		)}
 	</Container>
 );
 
