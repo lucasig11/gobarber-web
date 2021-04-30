88c5f98a71d17507c9382eb243b424bfd30a205d
lucas <lucasfonsecard@icloud.com>
2021-04-30 09:32:12 -0300

    add providers list page

--- src/pages/Providers/index.tsx
+++ src/pages/Providers/index.tsx
@@ -1,23 +1,98 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+import React, { useCallback, useEffect, useState } from 'react';
+import { parseISO, differenceInDays } from 'date-fns';
+import Spinner from 'react-bootstrap/Spinner';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-// import api from '../../services/apiClient';
+import { User } from '../../common/types';
+import api from '../../services/apiClient';
 import Header from '../../components/Header';
+import { usePopup } from '../../hooks/popup';
+import { useToast } from '../../hooks/toast';
 
-// import { useAuth } from '../../hooks/auth';
-// import { usePopup } from '../../hooks/popup';
-import { Container, Content } from './styles';
+import defaultAvatar from '../../assets/avatar_gobarber.png';
+
+import { Container, Content, ProvidersList, Provider } from './styles';
 
 const Dashboard: React.FC = () => {
-	// const { user, signOut } = useAuth();
-	// const { openPopup } = usePopup();
-	// States
-	// const [isLoading, setLoading] = useState(false);
+	const { openPopup } = usePopup();
+	const { addToast } = useToast();
+
+	const [isLoading, setLoading] = useState(false);
+	const [users, setUsers] = useState<User[]>([]);
+
+	const calcAccountAge = useCallback((date: string) => {
+		return differenceInDays(new Date(), parseISO(date));
+	}, []);
+
+	useEffect(() => {
+		api.get('/providers')
+			.then((response) => setUsers(response.data))
+			.catch(() =>
+				addToast({
+					type: 'error',
+					title: 'Erro ao carregar usuários.',
+					description:
+						'Houve um erro ao carregar os usuários, recarregue a página e tente novamente.',
+				}),
+			)
+			.finally(() => setLoading(false));
+	}, [addToast]);
 
 	return (
 		<>
 			<Container>
 				<Header />
-				<Content />
+				<Content>
+					<h1>Clientes e profissionais</h1>
+					{isLoading ? (
+						<Spinner animation="border" variant="secondary" />
+					) : (
+						<ProvidersList>
+							<div>
+								<div>
+									{users.length > 0 &&
+										users.map((user) => {
+											const {
+												avatar_url,
+												name,
+												id,
+											} = user;
+
+											return (
+												<Provider key={id}>
+													<img
+														src={
+															avatar_url ||
+															defaultAvatar
+														}
+														alt={name}
+														onClick={() => {
+															openPopup({ user });
+														}}
+													/>
+													<button
+														type="button"
+														onClick={() => {
+															openPopup({ user });
+														}}
+													>
+														<span>{name}</span>
+														<p>
+															{`Entrou há ${calcAccountAge(
+																user.created_at,
+															)} dias.`}
+														</p>
+													</button>
+												</Provider>
+											);
+										})}
+								</div>
+							</div>
+						</ProvidersList>
+					)}
+				</Content>
 			</Container>
 		</>
 	);
--- src/pages/Providers/styles.ts
+++ src/pages/Providers/styles.ts
@@ -1,3 +1,4 @@
+import { shade } from 'polished';
 import styled from 'styled-components';
 
 export const Container = styled.div``;
@@ -6,4 +7,79 @@ export const Content = styled.main`
 	max-width: 1120px;
 	margin: 64px auto;
 	display: flex;
+	flex-direction: column;
+	align-items: center;
+
+	> p {
+		margin: 0 auto 30px auto;
+		padding: 0;
+		width: 100%;
+		text-align: center;
+		border-bottom: 1px solid ${shade(0.5, '#999591')};
+	}
+
+	.spinner-border {
+		width: 80px;
+		height: 80px;
+		margin: 120px;
+	}
+`;
+
+export const ProvidersList = styled.div`
+	width: 90%;
+	overflow-y: auto;
+
+	> div {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		flex-wrap: wrap;
+		align-content: space-between;
+		margin: auto 0 auto 0;
+	}
+`;
+
+export const Provider = styled.div`
+	display: flex;
+	flex-direction: column;
+	float: left;
+	padding: 1%;
+	margin: 20px;
+	background-color: #28262e;
+	border-radius: 10px;
+	word-wrap: break-word;
+	align-items: center;
+
+	button {
+		border: 0;
+		background: none;
+	}
+
+	span {
+		color: #ff9000;
+		font-size: 20px;
+		display: block;
+		word-wrap: normal;
+		margin: 0 auto;
+		width: 190px;
+		&:hover {
+			color: white;
+		}
+	}
+
+	img {
+		margin: 15px;
+		object-fit: cover;
+		width: 120px;
+		height: 120px;
+		border-radius: 50%;
+		cursor: pointer;
+	}
+
+	p {
+		font-size: 14px;
+		margin: 0 auto;
+		justify-content: center;
+		color: ${shade(0.2, '#999591')};
+	}
 `;
